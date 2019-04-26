41f07cccbd637350f503246ce6278d95e94554d3
vladimir.parkhomenko <obhodny@gmail.com>
2019-04-26 23:50:48 +0300

    change of version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='qencode',
-    version='0.0.1',
+    version='0.9',
     description='Qencode Python SDK',
     url='https://github.com/qencode-dev/qencode-api-python-client',
     # url=here,
