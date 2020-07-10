11e6137ba4a129e48919c2a617c0a4aae23b1101
Tony Narlock <tony@git-pull.com>
2020-07-10 18:45:19 -0500

    Tweaks for setup.py, linting

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-"""A setuptools based setup module.
-"""
+"""A setuptools based setup module."""
+from os import path
 
 from setuptools import setup
-from os import path
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,12 +10,11 @@ with open('LONG_DESCRIPTION.md') as f:
 
 setup(
     name='qencode',
-    version='1.0.4',
-    description="Client library for main features and functionality of Qencode for Python v2.x.",
+    version='1.1.0',
+    description="Python bindings for the Qencode API",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/qencode-dev/qencode-api-python-client',
-    # url=here,
     author='Qencode Developer',
     author_email='team@qencode.com',
     license='proprietary',
